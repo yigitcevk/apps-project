b31f924eeb34eaa10afd86bb4919e5e481d0b029
Yiğit Çevik <yigit.cevikk@gmail.com>
2024-11-08 02:05:28 +0300

    first read operations and structure upgraded

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from starlette.responses import RedirectResponse
+
+from src.config.database import engine
+from src.util.db_dependency import get_db
+
+from src.routes import auth, users
+
+from src.routes.users import schemas, models
+from src.routes.auth import *
+
+users.models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Redirect / -> Swagger-UI documentation
+@app.get("/")
+def home():
+    """
+    # Redirect
+    to documentation (`/docs/`).
+    """
+    return RedirectResponse(url="/docs/")
+
+# Route to get all assets stored in the list.
+@app.get("/assets", response_model=List[users.schemas.Asset])
+async def get_assets(db: Session = Depends(get_db)):
+    return db.query(users.models.Asset).all()
+
+# Route to get all campaigns stored in the list.
+@app.get("/campaigns", response_model=List[users.schemas.Campaign])
+async def get_campaigns(db: Session = Depends(get_db)):
+    return db.query(users.models.Campaign).all()
+
+# Route to get all insights stored in the list.
+@app.get("/insights", response_model=List[users.schemas.Insight])
+async def get_insights(db: Session = Depends(get_db)):
+    return db.query(users.models.Insights).all()
+
+# Route to get all campaign_creatives stored in the list.
+@app.get("/campaign_creatives", response_model=List[users.schemas.CampaignCreative])
+async def get_insights(db: Session = Depends(get_db)):
+    return db.query(users.models.Insights).all()
